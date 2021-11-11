d510e99d9eaa83df52eaf4e50e2a264a8c8673dd
Joe Estrella <joe.estrella@xswap.ph>
2021-11-11 08:59:34 +0800

    Handle invalid test ID

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -42,12 +42,13 @@ class Test extends Command
     {
         $id = $this->argument('id');
 
-        if(!$id) dd('id is required');
-
         $sql = match ($id) {
             '1' => "SELECT CONCAT('T', LPAD(t.id, 11, 0)) AS ID, t.nickname AS Nickname, t.status AS Status, (SELECT GROUP_CONCAT( (SELECT CASE r.role WHEN 1 THEN 'Trainer' WHEN 2 THEN 'Assessor' WHEN 3 THEN 'Staff' END) SEPARATOR '/') FROM trn_teacher_role AS r WHERE r.teacher_id = t.id) AS Roles FROM `trn_teacher` AS t;",
             '2' => "SELECT t.id AS ID, t.nickname AS Nickname, SUM(IF(e.result = '1', 1, 0)) AS Taught, SUM(IF(e.result = '2', 1, 0)) AS NoShow, (SELECT COUNT(tt.status) AS Open FROM trn_time_table AS tt WHERE tt.teacher_id = t.id AND tt.status = '1') AS Open, (SELECT COUNT(tt.status) FROM trn_time_table AS tt WHERE tt.teacher_id = t.id AND tt.status = '3') AS Reserved FROM `trn_teacher` t JOIN trn_evaluation AS e ON e.teacher_id = t.id WHERE t.status <> '0' GROUP BY t.id;",
+            default => null
         };
+
+        if( !$sql ) dd('Invalid test ID: ' . $id);
 
         $this->do($sql);
     }
